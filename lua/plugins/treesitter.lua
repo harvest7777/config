87ed4b2a7a18546c4aee3fcd51c9ba8c227404dd
harvest7777 <333harvest.moon333@gmail.com>
2026-06-19 01:36:17 -0700

    switch to neovim-treesitter rewrite

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,18 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
+  'neovim-treesitter/nvim-treesitter',
+  dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
   lazy = false,
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
-  opts = {
-    ensure_installed = 'all',
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = '*',
+      callback = function()
+        local ok = pcall(vim.treesitter.start)
+        if not ok then return end
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo.foldmethod = 'expr'
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
