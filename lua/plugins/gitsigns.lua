73c610d1f51b8bd7fb14ebc535df1d4408eeb75b
harvest7777 <333harvest.moon333@gmail.com>
2026-06-19 01:51:21 -0700

    move hunk keymaps into gitsigns on_attach

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,14 @@
-return{
+return {
   'lewis6991/gitsigns.nvim',
-   opts = {
-     current_line_blame = true
-   },
+  opts = {
+    current_line_blame = true,
+    on_attach = function(bufnr)
+      local gs = require('gitsigns')
+      local map = function(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+      end
+      map('n', ']c', gs.next_hunk, 'Next hunk')
+      map('n', '[c', gs.prev_hunk, 'Prev hunk')
+    end,
+  },
 }
