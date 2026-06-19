497878bc0910ffc41e3951b2534881abca165d85
harvest7777 <333harvest.moon333@gmail.com>
2026-06-19 01:48:16 -0700

    delete empty startup buffer on first file open

--- init.lua
+++ init.lua
@@ -18,6 +18,25 @@ require('lazy').setup('plugins') -- scans lua/plugins/ automatically
 require("options")
 require("keymaps")
 
+-- delete the empty startup buffer once a real file is opened
+vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
+  once = true,
+  callback = function(ev)
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if buf ~= ev.buf
+        and vim.fn.buflisted(buf) == 1
+        and vim.api.nvim_buf_get_name(buf) == ''
+        and not vim.bo[buf].modified
+      then
+        local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+        if #lines == 0 or (#lines == 1 and lines[1] == '') then
+          pcall(vim.api.nvim_buf_delete, buf, {})
+        end
+      end
+    end
+  end,
+})
+
 -- lsps
 vim.lsp.config('lua_ls', {
   cmd = { 'lua-language-server' },
