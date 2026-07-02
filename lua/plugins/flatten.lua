85b96506f7e5c837092633985a9f5ae51292e415
harvest7777 <333harvest.moon333@gmail.com>
2026-07-02 12:31:23 -0700

    flatten

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -1,6 +1,17 @@
-return {
+return
+{
   'willothy/flatten.nvim',
   lazy = false,
   priority = 1001,
-  opts = {},
+  opts = {
+    window = { open = "current" },
+    hooks = {
+      pre_open = function()
+        if _G.lazygit_buf and vim.api.nvim_buf_is_valid(_G.lazygit_buf) then
+          vim.api.nvim_buf_delete(_G.lazygit_buf, { force = true })
+          _G.lazygit_buf = nil
+        end
+      end,
+    },
+  },
 }
