1f38012a7b46d5c3e467c3bab4f7cb3457aca322
harvest7777 <333harvest.moon333@gmail.com>
2026-07-02 11:52:50 -0700

    bind leader-d to gitsigns inline hunk preview

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,10 +5,12 @@ return {
     on_attach = function(bufnr)
       local gs = require('gitsigns')
       local map = function(mode, l, r, desc)
+        -- tset
         vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
       end
       map('n', ']c', gs.next_hunk, 'Next hunk')
       map('n', '[c', gs.prev_hunk, 'Prev hunk')
+      map('n', '<leader>d', gs.preview_hunk_inline, 'Preview hunk inline')
     end,
   },
 }
