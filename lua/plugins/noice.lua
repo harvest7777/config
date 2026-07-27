ca2c02d402a816f22539f107a9c701e2f43578af
harvest7777 <333harvest.moon333@gmail.com>
2026-07-26 18:07:06 -0700

    nocie

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,6 +2,16 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
+    -- Noice's built-in "popup" view (used by :Noice all/last, i.e. <leader>na
+    -- and <leader>nl) hardcodes size = { width = "120", height = "20" } --
+    -- absolute character counts, not percentages. On a window smaller than
+    -- that, Neovim just clamps the float to fit, which looks like it's
+    -- filling the whole window instead of scaling down. Percentages instead.
+    views = {
+      popup = {
+        size = { width = "80%", height = "70%" },
+      },
+    },
     messages = {
       enabled = true,
       view = "mini",        -- bottom-right corner instead of big popup
